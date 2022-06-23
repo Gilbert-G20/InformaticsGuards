f1c8d8a64173d42be59bb2f39a8e2e319f57cb60
Gilbert-G20 <ggomez@unimar.edu.ve>
2022-06-22 22:54:04 -0400

    estructuración del layout content

---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="side">
+        <h2>Sobre nosotros</h2>
+        <h5>Imagen de referencia:</h5>
+        <div class="fakeimg" style="height:200px;"></div>
+        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+        <h3>Más Info...</h3>
+        <p>Lorem ipsum dolor sit ame.</p>
+        <div class="fakeimg" style="height:60px;"></div><br>
+        <div class="fakeimg" style="height:60px;"></div><br>
+        <div class="fakeimg" style="height:60px;"></div>
+    </div>
+    <div class="main">
+        <h2>¿Qué ofrecemos?</h2>
+        <h5>Descripcion...</h5>
+        <div class="fakeimg" style="height:200px;"></div>
+        <p>Información...</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <br>
+        <h2>Información...</h2>
+        <h5>Descripción...</h5>
+        <div class="fakeimg" style="height:200px;"></div>
+        <p>Información...</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+</div>
