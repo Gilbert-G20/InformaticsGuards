9025e1b0127245f56e67347a0659c9ca5c5f4400
Gilbert-G20 <ggomez@unimar.edu.ve>
2022-06-22 22:47:11 -0400

    creación del header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<div class="header">
+    <h1>InformaticsGuards Spa | MaaS</h1>
+    <p>La <b>solución</b> a la organización de tu empresa.</p>
+</div>
+
+<div class="navbar">
+    <a href="#" class="active">Inicio</a>
+    <a href="#">Administrador</a>
+    <a href="#">Cliente</a>
+    <a href="#" class="right">Iniciar Sesion</a>
+    <a href="#" class="right">Registrar</a>
+</div>
