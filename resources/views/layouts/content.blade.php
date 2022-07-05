b8ad711d3ba6e1a487b07febf8f4831968d65967
Dgonzalez2001 <dgonzalez.5544@unimar.edu.ve>
2022-07-05 16:53:16 -0400

    Actualización de textos en 'content'

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -4,24 +4,16 @@
         
         <img class="Logo" src ='logoig.png'>
         
-        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+        <p>InformaticsGuards, es una empresa de desarrollo de software</p>
         <h3>Más Info...</h3>
-        <p>Lorem ipsum dolor sit ame.</p>
+        <p>Tenemos más de 10 años de experiencia en el mercado</p>
         <div class="fakeimg" style="height:60px;"></div><br>
         <div class="fakeimg" style="height:60px;"></div><br>
         <div class="fakeimg" style="height:60px;"></div>
     </div>
     <div class="main">
         <h2>¿Qué ofrecemos?</h2>
-        <h5>Descripcion...</h5>
         <div class="fakeimg" style="height:200px;"></div>
-        <p>Información...</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <br>
-        <h2>Información...</h2>
-        <h5>Descripción...</h5>
-        <div class="fakeimg" style="height:200px;"></div>
-        <p>Información...</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        <p>Ofrecemos una herramienta para el control, registros y modificación de turnos de horarios para los empleados de InformaticsGuards Spa</p>
     </div>
 </div>
