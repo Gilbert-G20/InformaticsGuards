e156b8d5229bf0333cf75885cc6ffa4e6f2ab7f2
Dgonzalez2001 <dgonzalez.5544@unimar.edu.ve>
2022-07-05 16:05:39 -0400

    Actualizacion footer/css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,9 @@
 <footer class="footer">
     Gonzalez D. | Gomez G.
+    <br> 
+    |Información de Contacto
     <br>
-    &copy;All Rights reserved.
+     informaticsguards@gmail.com | +58 424-8701858
+    <br>
+    &copy; 2022 InformaticsGuards. All Rights reserved.
 </footer>
