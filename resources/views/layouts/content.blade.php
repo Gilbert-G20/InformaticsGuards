908b05317978b9cc3d2195aa23503cb903851412
Dgonzalez2001 <dgonzalez.5544@unimar.edu.ve>
2022-07-05 16:58:47 -0400

    Agregando footer y header a todas las vistas

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -4,9 +4,9 @@
         
         <img class="Logo" src ='logoig.png'>
         
-        <p>InformaticsGuards, es una empresa de desarrollo de software</p>
+        <p>InformaticsGuards, es una empresa de desarrollo de software.</p>
         <h3>Más Info...</h3>
-        <p>Tenemos más de 10 años de experiencia en el mercado</p>
+        <p>Tenemos más de 10 años de experiencia en el mercado.</p>
         <div class="fakeimg" style="height:60px;"></div><br>
         <div class="fakeimg" style="height:60px;"></div><br>
         <div class="fakeimg" style="height:60px;"></div>
@@ -14,6 +14,6 @@
     <div class="main">
         <h2>¿Qué ofrecemos?</h2>
         <div class="fakeimg" style="height:200px;"></div>
-        <p>Ofrecemos una herramienta para el control, registros y modificación de turnos de horarios para los empleados de InformaticsGuards Spa</p>
+        <p>Ofrecemos una herramienta para el control, registros y modificación de turnos de horarios para los empleados de InformaticsGuards Spa.</p>
     </div>
 </div>
