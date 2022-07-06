84ddef155a8c8f2a1c171bec4b09bda71ddc8107
Gilbert-G20 <ggomez@unimar.edu.ve>
2022-07-06 17:01:48 -0400

    actualización del footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,12 +1,13 @@
 <footer class="footer">
-    Gonzalez D. | Gomez G.
-    <br> 
-    <br>
-    Información de Contacto
-    <br>
-    <br>
-    informaticsguards@gmail.com | +58 424-8701858
-    <br>
-    <br>
-    &copy; 2022 InformaticsGuards. All Rights reserved.
+    <div class="footer-content">
+        <span> Creado por: <br> Domingo González <br> Gilbert Gómez </span>
+        <span> &copy; 2022 InformaticsGuards. Derechos Reservados. </span>
+        <span>
+            <img src="{{ asset ('assets/email.png') }}" alt=""> infoguard@maas.com
+            <br>
+            <img src="{{ asset ('assets/phone.png') }}" alt=""> +58 424-8701858
+            <br>
+            <img src="{{ asset ('assets/location.png') }}" alt=""> Informatics Guards. MaaS. C.A. NE 6301, VE
+        </span>
+    </div>
 </footer>
