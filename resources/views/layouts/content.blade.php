156ab700e66ae89335c5a3f4033a4136f5540fa3
Dgonzalez2001 <dgonzalez.5544@unimar.edu.ve>
2022-07-13 23:26:19 -0400

    actualizando el content

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -5,13 +5,11 @@
         <img class="Logo" src ="{{ asset('assets/logoig.png') }}">
         
         <p>InformaticsGuards, es una empresa de desarrollo de software.</p>
-        <h3>Más Info...</h3>
-        <p>Tenemos más de 10 años de experiencia en el mercado.</p>
         <img class="img1" src ="{{ asset ('assets/programacion-web.png') }}">
     </div>
     <div class="main">
         <h2>¿Qué ofrecemos?</h2>
-        <div class="fakeimg" style="height:200px;"></div>
+        <img class="img1" src ="{{ asset ('assets/programacion-web.png') }}">
         <p>Ofrecemos una herramienta para el control, registros y modificación de turnos de horarios para los empleados de InformaticsGuards Spa.</p>
     </div>
 </div>
