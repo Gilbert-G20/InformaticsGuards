b9c80122fbc5c5ba866dac9ca602aaa6287c20eb
Dgonzalez2001 <dgonzalez.5544@unimar.edu.ve>
2022-07-13 22:12:29 -0400

    Construyendo vista register

--- resources/views/inicio/register.blade.php
+++ resources/views/inicio/register.blade.php
@@ -4,13 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app3.css') }}">
     <title>InformaticsGuards</title>
 </head>
 <body>
     @include ('layouts.header')
     <main>
-        
+        <form> action="/inicio/register" method ="POST">
+            @csrf
+            <label>DNI:</label>
+            <input type="text" name="DNI" placeholder="Su DNI">
+            <label>Nombre:</label>
+            <input type="text" name="nombre" placeholder="Su nombre">
+            <label>Apellido:</label>
+            <input type="text" name="apellido" placeholder="Su Apellido">
+            <label>Fecha de Nacimiento:;</label>
+            <input type="text" name="Fecha de Nacimiento" placeholder="Fecha de Nacimiento">
+            <label>Telefono:;</label>
+            <input type="text" name="Telefono" placeholder="Su Telefono">
+            <label>Contraseña:;</label>
+            <input type="text" name="Contraseña" placeholder="Su Contraseña">
+            <label>Role:;</label>
+            <input type="Role" name="Role" placeholder="Su Role">
+            <input type="submit" value="Guardar">
+        </form>
     </main>
     @include ('layouts.footer')
 </body>
