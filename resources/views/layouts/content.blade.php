f6ec00fd713266b8f0b57c4557d70a7cb715afd5
Gilbert-G20 <ggomez@unimar.edu.ve>
2022-07-13 23:49:29 -0400

    actualización en app.css, content y header

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -4,12 +4,15 @@
         
         <img class="Logo" src ="{{ asset('assets/logoig.png') }}">
         
-        <p>InformaticsGuards, es una empresa de desarrollo de software.</p>
-        <img class="img1" src ="{{ asset ('assets/programacion-web.png') }}">
+        <p>InformaticsGuards, es una empresa de desarrollo de software. <br><br> Tenemos más de 10 años de experiencia en el mercado.</p>
     </div>
     <div class="main">
         <h2>¿Qué ofrecemos?</h2>
-        <img class="img1" src ="{{ asset ('assets/programacion-web.png') }}">
+        <div class="img-col">
+            <img class="img1" src ="{{ asset ('assets/programacion-web.png') }}">
+            <img class="img2" src ="{{ asset ('assets/presentacion.png') }}">
+            <img class="img3" src ="{{ asset ('assets/computadora.png') }}">
+        </div>
         <p>Ofrecemos una herramienta para el control, registros y modificación de turnos de horarios para los empleados de InformaticsGuards Spa.</p>
     </div>
 </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,7 @@
 </div>
 
 <div class="navbar">
-    <a href="#" class="active">Inicio</a>
-    <a href="#">Administrador</a>
-    <a href="#">Cliente</a>
+    <a href="#" class="left">Inicio</a>
     <a href="#" class="right">Iniciar Sesion</a>
     <a href="#" class="right">Registrar</a>
 </div>
