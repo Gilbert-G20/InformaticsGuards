f01dbd6d557d4f66f5b6c6dcf29aa3536f076f1d
Gilbert-G20 <ggomez@unimar.edu.ve>
2022-07-16 16:21:57 -0400

    actualización para el header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,10 +1,7 @@
-<div class="header">
-    <h1>InformaticsGuards Spa | MaaS</h1>
-    <p>La <b>solución</b> a la organización de tu empresa.</p>
-</div>
-
-<div class="navbar">
-    <a href="{{ url('/') }}" class="left">Inicio</a>
-    <a href="{{ url('/inicio/login') }}" class="right">Iniciar Sesion</a>
-    <a href="{{ url('/inicio/register') }}" class="right">Registrar</a>
-</div>
+<header class="header">
+    <nav class="navbar">
+        <a href="{{ url('/') }}" class="left">InformaticsGuards | MaaS</a>
+        <a href="{{ url('/inicio/login') }}" class="right">Iniciar Sesion</a>
+        <a href="{{ url('/inicio/register') }}" class="right">Registrar</a>
+    </nav>
+</header>
